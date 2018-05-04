c53e89acc4c05dea962abf31c09f7afcddd364c1
Gram <master_fess@mail.ru>
2018-05-04 11:08:53 +0500

    +some constants

---
+++ djgpp/constants.py
@@ -0,0 +1,26 @@
+
+# https://developer.android.com/reference/android/Manifest.permission_group
+GROUPS = {
+    'calendar': [],
+    'camera': [],
+    'contacts': ['accounts'],
+    # https://github.com/r3gis3r/SampleCSipSimpleApp/blob/master/SampleCSipSimpleApp/AndroidManifest.xml
+    'cost money': ['sip'],
+    'location': [],
+    'microphone': ['audio'],
+    # why android manifest doesn't have this category?
+    'network': ['connections', 'wifi', 'bluetooth'],
+    'phone': ['call'],
+    'sensors': ['vibrate'],
+    'sms': ['mms', 'cell'],
+    'storage': ['usb'],
+    # https://github.com/chrislacy/LauncherJellyBean/blob/master/AndroidManifest.xml
+    'system tools': ['settings', 'uninstall', 'install'],
+}
+
+UPPERCASE = {
+    'gps': 'GPS',
+    'sms': 'SMS',
+    'mms': 'MMS',
+    'WIFI': 'Wi-Fi',
+}
